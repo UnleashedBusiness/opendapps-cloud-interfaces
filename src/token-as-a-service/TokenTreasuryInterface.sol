d51d88af505bf6a198b6a0e2a6df6b1aedc451ae
ShadowsCrypto <ceo@opendapps.cloud>
2023-09-08 22:54:39 +0300

    fixed treasury itnefaces

--- src/token-as-a-service/TokenTreasuryInterface.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.7;
-
-interface TokenTreasuryInterface {
-    function isTokenListedOnDex(address router) external view returns (bool);
-    function addLiquidityV2(address router, address weth, uint256 tokenLiquidutyAmount, uint256 ethLiquidityAmount) external payable;
-}
