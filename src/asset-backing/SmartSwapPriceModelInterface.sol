903d395a25727a88f4f3574966ebef678fe87180
ShadowsCrypto <ceo@opendapps.cloud>
2023-09-12 22:48:50 +0300

    fixed backing model price interface

--- src/asset-backing/SmartSwapPriceModelInterface.sol
+++ src/asset-backing/SmartSwapPriceModelInterface.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.7;
 
 interface SmartSwapPriceModelInterface {
-    function quoteBurn(uint256 backingSize, uint256 liquidity, uint256 totalSupply) external view returns (uint256);
-    function quoteFlip(uint256 backingSize, uint256 liquidity, uint256 totalSupply) external view returns (uint256);
+    function quoteBurn(uint256 backingSize, uint256 smartBurnAmount, uint256 liquidity, uint256 totalSupply) external view returns (uint256);
+    function quoteFlip(uint256 backingSize, uint256 smartBurnAmount, uint256 liquidity, uint256 totalSupply) external view returns (uint256);
 }
