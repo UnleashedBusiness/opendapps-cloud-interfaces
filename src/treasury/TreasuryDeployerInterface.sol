0b4ba41279299afd73f2ee08fb6fb468de87631f
ShadowsCrypto <ceo@opendapps.cloud>
2024-02-24 20:46:44 +0200

    fix(ODAPPS-343): treasury deployer interface renamed properly

--- src/treasury/TreasuryDeployerInterface.sol
+++ src/treasury/TreasuryDeployerInterface.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.7;
 
-interface TokenAsAServiceDeployerInterface {
+interface TreasuryDeployerInterface {
     function operations() external view returns (address[] memory);
 
     function enableOperation(address operation) external;
