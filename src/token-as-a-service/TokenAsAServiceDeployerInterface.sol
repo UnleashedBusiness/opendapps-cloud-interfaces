a0147788f03cae9e72422453f40bafd9398013bd
ShadowsCrypto <ceo@opendapps.cloud>
2024-03-26 16:31:44 +0200

    feat(ODAPPS-294): token deployer deploy methods merge

--- src/token-as-a-service/TokenAsAServiceDeployerInterface.sol
+++ src/token-as-a-service/TokenAsAServiceDeployerInterface.sol
@@ -9,30 +9,39 @@ interface TokenAsAServiceDeployerInterface {
         address treasury;
     }
 
+    struct TokenDeploymentBaseInputs {
+        string name;
+        string ticker;
+        uint256 supply;
+        string metadataUrl;
+    }
+
+    struct TokenDeploymentInflationInputs {
+        bool enabled;
+        uint256 initialSupplyPercent;
+        uint256 rewardRounds;
+        uint256 blockPerCycle;
+    }
+
     function minEthLiquidityAmount() external view returns (uint256);
+
     function ownerRewardsReleaseBlocks() external view returns (uint256);
+
     function ownerRewardCycles() external view returns (uint256);
+
     function ownershipNFTCollection() external view returns (address);
 
     function weth(address router) external view returns (address);
+
     function availableDexRouters() external view returns (address[] memory);
 
-    function deployBasicToken(
-        string calldata name, string calldata ticker, uint256 supply, bytes32 refCode
+    function deployToken(
+        TokenDeploymentBaseInputs calldata tokenBaseInputs,
+        TokenDeploymentInflationInputs calldata tokenInflationInputs,
+        bytes32 refCode
     ) payable external returns (TokenDeployment memory);
-
-    function deployHardCapToken(
-        string calldata name, string calldata ticker, uint256 supply, uint256 ownerAmount,
-        bool complexTax, string calldata metadataUrl, bytes32 refCode
-    ) payable external returns (TokenDeployment memory);
-
-    function deployInflationaryToken(
-        string calldata name, string calldata ticker, uint256 maxSupply,
-        uint256 initialSupplyPercent, uint256 rewardRounds, uint256 blockPerCycle,
-        uint256 ownerAmount, string calldata metadataUrl, bytes32 refCode
-    ) payable external returns (TokenDeployment memory);
-
 
     function upgradeTokenomics(address tokenomics) payable external;
+
     function upgradeInflation(address inflation) payable external;
 }
