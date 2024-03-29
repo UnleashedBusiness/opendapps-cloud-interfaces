c997fcc97e35b369e42d241da9eaee8facd1f5dd
ShadowsCrypto <ceo@opendapps.cloud>
2024-03-29 22:47:53 +0200

    vesting interface

---
+++ src/vesting/VestingInterface.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.7;
+
+interface VestingInterface {
+    function totalRewards(address token) external view returns (uint256);
+    function rewards(address token, address wallet) external view returns (uint256);
+
+    function availableTotalRewards(address token) external view returns (uint256);
+    function availableRewards(address token, address wallet) external view returns (uint256);
+
+    function remainingTotalRewards(address token) external view returns (uint256);
+    function remainingRewards(address token, address wallet) external view returns (uint256);
+
+    function totalRewardsPerReleaseCycle(address token) external view returns (uint256);
+    function rewardsPerReleaseCycle(address wallet, address token) external view returns (uint256);
+
+    function totalReleaseCycles() external view returns (uint256);
+    function releaseCycleBlockLength() external view returns (uint256);
+
+    function lockHeight() external view returns (uint256);
+    function finalReleaseHeight() external view returns (uint256);
+
+    function rewardTokens() external view returns (address[]);
+    function payees() external view returns (address[], uint256[]);
+    function payeePercent(address wallet) external view returns (uint256);
+
+    function withdraw(address token, uint256 amount) external;
+    function claim(address token, uint256 amount) external;
+    function begin(address[] calldata tokens, address[] payees, uint256[] payeePercents, uint256 rewardCycles, uint256 blocksPerCycle) external;
+    function enableToken(address token) external;
+}
