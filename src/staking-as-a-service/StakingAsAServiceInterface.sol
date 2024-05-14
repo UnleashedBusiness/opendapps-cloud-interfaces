f1250c898b25177273102e9f62b87d5285a63290
ShadowsCrypto <ceo@opendapps.cloud>
2024-05-14 16:48:32 +0300

    missing saas interface shares method

--- src/staking-as-a-service/StakingAsAServiceInterface.sol
+++ src/staking-as-a-service/StakingAsAServiceInterface.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 interface StakingAsAServiceInterface {
     function stakingToken() external view returns(address);
     function availableRewards(address owner, address token) external view returns(uint256);
+    function accountShares(address account) external view returns (uint256);
 
     function withdrawRewards(address token) external;
     function deposit(uint256 amount) external;
