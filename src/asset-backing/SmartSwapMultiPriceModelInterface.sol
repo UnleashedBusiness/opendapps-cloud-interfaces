196774489860d5d5d53f4ad9094e96ae991d1a9a
ShadowsCrypto <ceo@opendapps.cloud>
2024-05-21 13:22:19 +0300

    Revert "fixed multi-asset flip price quote"
    This reverts commit 913767751c9fc1c27294dfd0226b18260f0d5445.

--- src/asset-backing/SmartSwapMultiPriceModelInterface.sol
+++ src/asset-backing/SmartSwapMultiPriceModelInterface.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.7;
 
 interface SmartSwapMultiPriceModelInterface {
     function quoteBurn(uint256[] memory backingSize, uint256 smartBurnAmount, uint256 liquidity, uint256 totalSupply) external view returns (uint256[] memory);
-    function quoteFlip(uint256[] memory backingSize, uint256 smartBurnAmount, uint256 liquidity, uint256 totalSupply) external view returns (uint256 memory);
+    function quoteFlip(uint256[] memory backingSize, uint256 smartBurnAmount, uint256 liquidity, uint256 totalSupply) external view returns (uint256[] memory);
 }
