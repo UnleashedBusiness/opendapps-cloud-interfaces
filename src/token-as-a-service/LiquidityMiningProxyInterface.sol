0d79efc76efa109f5bee07b4fede93440c48aae3
ShadowsCrypto <ceo@opendapps.cloud>
2024-06-25 16:51:39 +0300

    fix: missing payable on addLP

--- src/token-as-a-service/LiquidityMiningProxyInterface.sol
+++ src/token-as-a-service/LiquidityMiningProxyInterface.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.7;
 interface LiquidityMiningProxyInterface {
     function toggleV2PairWhitelist(address router, address pairedAsset) external;
 
-    function addLiquidity(address router, address pairedAsset, uint256 tokenAmount, uint256 pairedAssetAmount, uint256 expectedLPTokens) external;
+    function addLiquidity(address router, address pairedAsset, uint256 tokenAmount, uint256 pairedAssetAmount, uint256 expectedLPTokens) payable external;
     function removeLiquidity(address router, address pairedAsset, uint256 lpTokens, uint256 expectedTokenAmount, uint256 expectedPairedAssetAmount) external;
 }
