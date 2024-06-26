82f7c34e0204ba898399a49ed7ce21f538c442ae
ShadowsCrypto <ceo@opendapps.cloud>
2024-06-26 15:57:33 +0300

    feat: lm pair list method

--- src/token-as-a-service/LiquidityMiningProxyInterface.sol
+++ src/token-as-a-service/LiquidityMiningProxyInterface.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 interface LiquidityMiningProxyInterface {
+    function allowedPairsForRouter(address router) external view returns(address[] memory);
     function toggleV2PairWhitelist(address router, address pairedAsset) external;
 
     function addLiquidity(address router, address pairedAsset, uint256 tokenAmount, uint256 pairedAssetAmount, uint256 expectedLPTokens) payable external;
