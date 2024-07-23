493a8d03da6816b97be1ecb83182175786b54952
ShadowsCrypto <ceo@opendapps.cloud>
2024-07-23 22:35:26 +0300

    fix: wrong toggleRefCodeWhiteListMode signature

--- src/deployer/ReferralsEngineInterface_v2.sol
+++ src/deployer/ReferralsEngineInterface_v2.sol
@@ -8,7 +8,7 @@ interface ReferralsEngineInterface_v2 is IReferralsEngine {
     function getTaxationReceivers(bytes32 refCode, address sender) external view returns (uint256[] memory percents, address[] memory receiversArray);
 
     function toggleAddressToRefCodeWhitelist(bytes32 refCode, address user) external;
-    function toggleRefCodeWhiteListMode(bytes32 refCode, address user) external;
+    function toggleRefCodeWhiteListMode(bytes32 refCode) external;
 
     function setDefaultReceiver(address target) external;
     function assignExtendedRefCode(bytes32 refCode, address[] memory receiver, uint256[] memory customSizes) external;
